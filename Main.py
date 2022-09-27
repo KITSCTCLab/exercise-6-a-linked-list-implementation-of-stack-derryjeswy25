57b6dc106050ec0f12d39cb2e2a0ad5a197b9fb3
derryjeswy25 <109648036+derryjeswy25@users.noreply.github.com>
2022-09-27 21:55:07 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,34 +1,37 @@
-
-  def pop(self) -> None:
-    # Write your code here
-     if(self.head is not None):
-          self.head = self.head.next
-
-  def status(self):
-    """
-    It prints all the elements of stack.
-    """
-    # Write your code here  
-    ptr = self.head
-    if(self.head is None):
-        print("None")
-    else:
-        while (ptr):
-            print(ptr.data, end="=>")
-            ptr = ptr.next
-        print("None")
-
-
+class Node:
+def __init__(self, data):
+self.data = data
+self.next = None
+class Stack:
+def __init__(self):
+self.head = None
+def push(self, data) -> None:
+nn = Node(data)
+nn.next = self.head
+self.head = nn
+def pop(self) -> None:
+if(self.head!=None):
+temp = self.head
+self.head = temp.next
+def status(self):
+if(self.head != None):
+temp = self.head
+while temp.next!= None:
+print(temp.data, end = "")
+print("=>", end = "")
+temp = temp.next
+print(temp.data, end = "=>")
+print("None")
 # Do not change the following code
 stack = Stack()
 operations = []
 for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
+operations.append(specific_operation.strip())
 input_data = input()
 data = input_data.split(',')
 for i in range(len(operations)):
-  if operations[i] == "push":
-    stack.push(int(data[i]))
-  elif operations[i] == "pop":
-    stack.pop()
+if operations[i] == "push":
+stack.push(int(data[i]))
+elif operations[i] == "pop":
+stack.pop()
 stack.status()
